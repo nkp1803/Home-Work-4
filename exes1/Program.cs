67efbb69e671db70a8a12978f5d517cd837ad735
konstantin <nkp.yalta@mail.ru>
2022-06-21 22:51:17 +0300

    add exes1

---
+++ exes1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+System.Console.WriteLine("Введите число A: ");
+string ia = Console.ReadLine();
+int a = int.Parse(ia);
+System.Console.WriteLine("Введите число B: ");
+string ib = Console.ReadLine();
+int b = int.Parse(ib);
+int i = 0;
+long p = 1;
+
+for (i = 0; i < b; i++)
+{
+    p = p * a;
+}
+System.Console.WriteLine($"{a} в степени {b} равно {p}");
