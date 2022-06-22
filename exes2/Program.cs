ba7cc77b869630294914244243919ca2c8346e6a
konstantin <nkp.yalta@mail.ru>
2022-06-22 23:26:32 +0300

    add exes2

---
+++ exes2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+System.Console.WriteLine("Введите число: ");
+string istring = Console.ReadLine();
+int inum = int.Parse(istring);
+int lenght = istring.Length;
+int[] arr = new int[lenght];
+int index = 0;
+int sum = 0;
+for (index = 0; index < lenght; index++)
+{
+    var tmp = inum % 10;
+    inum = inum / 10;
+    sum = sum + tmp;
+}
+System.Console.WriteLine($"Сумма цифр числа{istring}равна{sum}");
